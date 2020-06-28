299027f3b2d868a9f58126090284448da0491d51
zhaojun <347436604@qq.com>
2020-06-28 13:55:21 +0800

    kafka demo

---
+++ my7un/src/main/java/top/i7un/springboot/mytest/KafkaLisener.java
@@ -0,0 +1,19 @@
+package top.i7un.springboot.mytest;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Noone on 2020/6/28.
+ */
+@Component
+public class KafkaLisener {
+
+    @KafkaListener(topics = "my_test_topic")
+    public void lesten(ConsumerRecord<?,?> record){
+        System.out.println(111);
+        System.out.println(record.topic()+record.key()+record.value());
+        System.out.println(222);
+    }
+}
---
+++ my7un/src/main/java/top/i7un/springboot/mytest/KafkaTest.java
@@ -0,0 +1,22 @@
+package top.i7un.springboot.mytest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Noone on 2020/6/28.
+ */
+@RequestMapping("/kafka")
+@RestController
+public class KafkaTest {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+    @RequestMapping("/test")
+    public String testKafka(){
+        kafkaTemplate.send("my_test_topic","1","a");
+        return "ok";
+    }
+}
