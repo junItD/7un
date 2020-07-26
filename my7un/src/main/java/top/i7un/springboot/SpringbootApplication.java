62bca6ac8cb6c4ae75322c94a6595bd885880674
zhaojun <347436604@qq.com>
2020-07-26 23:57:36 +0800

    自动装配

--- my7un/src/main/java/top/i7un/springboot/SpringbootApplication.java
+++ my7un/src/main/java/top/i7un/springboot/SpringbootApplication.java
@@ -4,12 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import top.i7un.springboot.mytest.SpringBeanPojo;
+import top.i7un.springboot.mytest.Start;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -31,6 +34,16 @@ public class SpringbootApplication {
         ConfigurableApplicationContext run = SpringApplication.run(SpringbootApplication.class, args);
         SpringBeanPojo springbeanpojo = (SpringBeanPojo) run.getBean("top.i7un.springboot.mytest.SpringBeanPojo");
         System.out.println(springbeanpojo.getName());
+
+//        javaconfig的斑斑
+//        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Start.class);
+//        String[] beanDefinitionNames = annotationConfigApplicationContext.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            System.out.println(beanDefinitionName);
+//        }
+//            xml的版本
+//        ClassPathXml ApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("xxx.xml");
+
 
     }
 
